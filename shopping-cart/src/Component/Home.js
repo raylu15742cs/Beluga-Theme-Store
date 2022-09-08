ce8bfc17f834db70990bbb37d09307abce56e0e8
raylu15742cs <raylucs15742@gmail.com>
2022-09-07 20:09:13 -0700

    add beluga image

--- shopping-cart/src/Component/Home.js
+++ shopping-cart/src/Component/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <img  className="belugaimg" src={require(`../img/beluga.png`)} alt="beluga"></img>
     </div>
   );
 };
--- shopping-cart/src/Component/Nav.js
+++ shopping-cart/src/Component/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
         </div>  
         <div className="leftnav">
             <Link to="/shop" className="shop">Shop</Link>
-            <Link onClick={()=> setScore(score+1)} to="/checkout"><img src={require("./bag.png")} alt="hi"></img></Link>
+            <Link onClick={()=> setScore(score+1)} to="/checkout"><img className="bag" src={require("../img/bag.png")} alt="hi"></img></Link>
             <p onClick={()=> setScore(score+1)} className="insidebox">{score}</p>
         </div>
       </nav>
