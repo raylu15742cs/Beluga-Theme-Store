1a797cca37cba3aca278687c522276c41cd2afcf
raylu15742cs <raylucs15742@gmail.com>
2022-09-08 16:51:43 -0700

    update images sizing and home div positioning

--- shopping-cart/src/Component/Home.js
+++ shopping-cart/src/Component/Home.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Yeezy 350 Beluga</h1>
-      <Link to="/shop" className="shops"><button >Shop Now</button></Link>
+      <div className="leftsidehome">
+        <h1>Yeezy 350 Beluga</h1>
+        <Link to="/shop" className="shops"><button >Shop Now</button></Link>
+      </div>
       <img  className="belugaimg" src={require(`../img/beluga.png`)} alt="beluga"></img>
     </div>
   );
