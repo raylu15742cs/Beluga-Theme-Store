0c37dab51c6822b85e58b7642caef4ee6d0241f9
raylu15742cs <raylucs15742@gmail.com>
2022-09-12 10:43:05 -0700

    updates and remove tab icon

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,15 +1,34 @@
-import Home from "./Component/Home";
-import Shop from './Component/Shop';
+import { Routes, Route } from 'react-router-dom';
 import Checkout from './Component/Checkout';
+import Home from './Component/Home';
+import Shop from "./Component/Shop"
+import Nav from './Component/Nav';
+import Footer from './Component/Footer';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Home />
-      <Shop />
-      <Checkout />
-    </div>
-  );
+const App = () => {
+    const [score, setScore] = useState(0);
+    const [shopCart , setShopCart] = useState({})
+    const updatescore = () => {setScore(score+1)}
+    const clearScore = () => {
+      setScore(score-score)
+      Object.keys(shopCart).forEach((k) => delete shopCart[k])
+      console.log('clear')
+    }
+    useEffect(() => {
+      console.log(shopCart)
+    })
+    return (
+      <div>
+          <Nav score={score} setScore={updatescore}/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop  score={score} setScore={updatescore} setShopCart={setShopCart} shopCart={shopCart}/>} />
+            <Route path="/checkout" element={<Checkout shopCart={shopCart} clearScore={clearScore} />} />
+          </Routes>
+          <Footer />
+      </div>
+    );
 }
 
 export default App;
