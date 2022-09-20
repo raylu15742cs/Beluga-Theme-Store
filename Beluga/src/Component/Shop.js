9f5e1a605bd75e04d2ec924714cfbfacb4e0cfa6
raylu15742cs <raylucs15742@gmail.com>
2022-09-20 14:30:52 -0700

    add image carousel to shop

--- Beluga/src/Component/Shop.js
+++ Beluga/src/Component/Shop.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import React from 'react';
+import Carousel from "better-react-carousel";
 
 const Shop = ( { score, shopCart, setShopCart ,setScore} ) => {
   const test = () => {
@@ -12,13 +14,35 @@ const Shop = ( { score, shopCart, setShopCart ,setScore} ) => {
   }
   return (
     <div className="shop">
-      <img src={require(`../img/beluga.png`)} alt="beluga"></img>
+      <Carousel cols={1} rows={1} loop>
+        <Carousel.Item>
+          <img src={require(`../img/beluga.png`)} alt="beluga"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={require(`../img/beluga.png`)} alt="beluga"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={require(`../img/beluga.png`)} alt="beluga"></img>
+        </Carousel.Item>
+      </Carousel>
       <div className="shopinside">
         <h1>Yeezy Beluga 350 V2</h1>
         <h3>Product Description</h3>
-        <p> One of Yeezy's most celebrated designs received a reflective makeover with the adidas Yeezy Boost 350 V2 Beluga Reflective.</p>
-        <p>The adidas Yeezy Boost 350 V2 Beluga Reflective builds off of the original Beluga colorway by adding reflective qualities and speckled orange accents to its Primeknit upper. Signature details like a Boost sole and orange side stripe complete the design.</p>
-        <p>The adidas Yeezy Boost 350 V2 Beluga Reflective released in December of 2021 and retailed for $240.</p>
+        <p>
+          {' '}
+          One of Yeezy's most celebrated designs received a reflective makeover
+          with the adidas Yeezy Boost 350 V2 Beluga Reflective.
+        </p>
+        <p>
+          The adidas Yeezy Boost 350 V2 Beluga Reflective builds off of the
+          original Beluga colorway by adding reflective qualities and speckled
+          orange accents to its Primeknit upper. Signature details like a Boost
+          sole and orange side stripe complete the design.
+        </p>
+        <p>
+          The adidas Yeezy Boost 350 V2 Beluga Reflective released in December
+          of 2021 and retailed for $240.
+        </p>
         <h3>Price: $240</h3>
         <select id="language">
           <option value="4">4</option>
